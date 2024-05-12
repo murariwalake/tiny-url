e04916094d47bf9e3d28fed9ddd0ff2d22e425e6
tejaswinidr <tejaswinidr97@gmail.com>
2024-05-12 19:49:05 +0530

    splitted end points into two APIs

--- src/main/java/com/murariwalake/tinyurl/api/TinyUrlController.java
+++ src/main/java/com/murariwalake/tinyurl/api/TinyUrlController.java
@@ -12,15 +12,11 @@ import org.apache.zookeeper.ZooKeeper;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
 @Slf4j
+@RestController
+@RequestMapping("/api/v1/tinyUrl")
 public class TinyUrlController {
     private final TinyUrlService tinyUrlService;
     private final ZooKeeper zooKeeper;
@@ -47,12 +43,6 @@ public class TinyUrlController {
         String tinyUrl = String.format("http://%s:%s/%s", host, port, tinyUrlKey);
 
         return ResponseEntity.ok(Map.of("tinyUrl", tinyUrl));
-    }
-
-    @GetMapping("/{tinyUrl}")
-    public ResponseEntity getLongUrl(@PathVariable String tinyUrl) {
-        String longUrl = tinyUrlService.getLongUrl(tinyUrl);
-        return ResponseEntity.status(302).header("Location", longUrl).build();
     }
 
     @GetMapping
---
+++ src/main/java/com/murariwalake/tinyurl/api/UrlRedirectController.java
@@ -0,0 +1,37 @@
+
+package com.murariwalake.tinyurl.api;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.murariwalake.tinyurl.model.TinyUrlEntity;
+import com.murariwalake.tinyurl.service.TinyUrlService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooKeeper;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class UrlRedirectController {
+    private final TinyUrlService tinyUrlService;
+
+    public UrlRedirectController(final TinyUrlService tinyUrlService) {
+        this.tinyUrlService = tinyUrlService;
+    }
+
+    @GetMapping("/{tinyUrl}")
+    public ResponseEntity getLongUrl(@PathVariable String tinyUrl) {
+        String longUrl = tinyUrlService.getLongUrl(tinyUrl);
+        return ResponseEntity.status(302).header("Location", longUrl).build();
+    }
+}
